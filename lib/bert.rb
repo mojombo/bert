e77dfa43f239cb401f80de15caf9e9e8394d3917
Tom Preston-Werner <tom@mojombo.com>
2009-10-07 21:57:57 -0700

    add BERT.ebin to facilitate Erlang equivalency testing

--- lib/bert.rb
+++ lib/bert.rb
@@ -12,4 +12,10 @@ module BERT
   def self.decode(bert)
     Decoder.decode(bert)
   end
+
+  def self.ebin(str)
+    bytes = []
+    str.each_byte { |b| bytes << b.to_s }
+    "<<" + bytes.join(',') + ">>"
+  end
 end
--- test/bert_test.rb
+++ test/bert_test.rb
@@ -13,5 +13,11 @@ class BertTest < Test::Unit::TestCase
       after = [:user, {:name => 'TPW', :nick => 'mojombo'}]
       assert_equal after, BERT.decode(before)
     end
+
+    should "ebin" do
+      before = "\203h\002d\000\004userh\003d\000\004dicth\002d\000\004namem\000\000\000\003TPWh\002d\000\004nickm\000\000\000\amojombo"
+      after = "<<131,104,2,100,0,4,117,115,101,114,104,3,100,0,4,100,105,99,116,104,2,100,0,4,110,97,109,101,109,0,0,0,3,84,80,87,104,2,100,0,4,110,105,99,107,109,0,0,0,7,109,111,106,111,109,98,111>>"
+      assert_equal after, BERT.ebin(before)
+    end
   end
 end
