15c990c1aafd360ab35ecb1dfc120fd2c76ea783
Tom Preston-Werner <tom@mojombo.com>
2009-10-07 22:34:59 -0700

    make tests deterministic

--- test/bert_test.rb
+++ test/bert_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class BertTest < Test::Unit::TestCase
   context "BERT" do
     should "encode" do
-      before = [:user, {:name => 'TPW', :nick => 'mojombo'}]
-      after = "\203h\002d\000\004userh\003d\000\004dicth\002d\000\004namem\000\000\000\003TPWh\002d\000\004nickm\000\000\000\amojombo"
+      before = [:user, {:name => 'TPW'}]
+      after = "\203h\002d\000\004userh\002d\000\004dicth\002d\000\004namem\000\000\000\003TPW"
       assert_equal after, BERT.encode(before)
     end
 
     should "decode" do
-      before = "\203h\002d\000\004userh\003d\000\004dicth\002d\000\004namem\000\000\000\003TPWh\002d\000\004nickm\000\000\000\amojombo"
-      after = [:user, {:name => 'TPW', :nick => 'mojombo'}]
+      before = "\203h\002d\000\004userh\002d\000\004dicth\002d\000\004namem\000\000\000\003TPW"
+      after = [:user, {:name => 'TPW'}]
       assert_equal after, BERT.decode(before)
     end
 
