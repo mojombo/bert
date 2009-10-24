f0524d23c838d2d6cee0e450ad5cd8a9557a96f2
Tom Preston-Werner <tom@mojombo.com>
2009-10-24 12:01:18 -0700

    eliminate unneeded array traversal and update benches

--- bench/bench.rb
+++ bench/bench.rb
@@ -14,18 +14,21 @@ large = ["abc" * 1000] * 100
 complex = [42, {:foo => 'bac' * 100}, t[(1..100).to_a]] * 10
 
 Benchmark.bm do |bench|
-  bench.report("JSON tiny") {ITER.times {JSON.load(JSON.dump(tiny))}}
-  bench.report("JSON small") {ITER.times {JSON.load(JSON.dump(small))}}
-  bench.report("JSON large") {ITER.times {JSON.load(JSON.dump(large))}}
-  bench.report("JSON complex") {ITER.times {JSON.load(JSON.dump(complex))}}
-  bench.report("YAJL tiny") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(tiny))}}
-  bench.report("YAJL small") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(small))}}
-  bench.report("YAJL large") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(large))}}
-  bench.report("YAJL complex") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(complex))}}
   bench.report("BERT tiny") {ITER.times {BERT.decode(BERT.encode(tiny))}}
   bench.report("BERT small") {ITER.times {BERT.decode(BERT.encode(small))}}
   bench.report("BERT large") {ITER.times {BERT.decode(BERT.encode(large))}}
   bench.report("BERT complex") {ITER.times {BERT.decode(BERT.encode(complex))}}
+
+  bench.report("JSON tiny") {ITER.times {JSON.load(JSON.dump(tiny))}}
+  bench.report("JSON small") {ITER.times {JSON.load(JSON.dump(small))}}
+  bench.report("JSON large") {ITER.times {JSON.load(JSON.dump(large))}}
+  bench.report("JSON complex") {ITER.times {JSON.load(JSON.dump(complex))}}
+
+  bench.report("YAJL tiny") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(tiny))}}
+  bench.report("YAJL small") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(small))}}
+  bench.report("YAJL large") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(large))}}
+  bench.report("YAJL complex") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(complex))}}
+
   bench.report("Ruby tiny") {ITER.times {Marshal.load(Marshal.dump(tiny))}}
   bench.report("Ruby small") {ITER.times {Marshal.load(Marshal.dump(small))}}
   bench.report("Ruby large") {ITER.times {Marshal.load(Marshal.dump(large))}}
--- lib/bert/decoder.rb
+++ lib/bert/decoder.rb
@@ -5,20 +5,7 @@ module BERT
     #
     # Returns a Ruby object
     def self.decode(bert)
-      simple_ruby = Decode.decode(bert)
-      convert(simple_ruby)
-    end
-
-    # Convert simple Ruby form into complex Ruby form.
-    #   +item+ is the Ruby object to convert
-    #
-    # Returns the converted Ruby object
-    def self.convert(item)
-      if item.instance_of?(Array)
-        item.map { |x| convert(x) }
-      else
-        item
-      end
+      Decode.decode(bert)
     end
   end
 end
